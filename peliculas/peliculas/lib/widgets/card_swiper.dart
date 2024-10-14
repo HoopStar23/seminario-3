6f183837a0dfe816ce7adb4cb0b16255d640947c
Hakeem <sb.hakeem99@gmail.com>
2024-10-14 17:05:19 +0200

    Cards

--- peliculas/peliculas/lib/widgets/card_swiper.dart
+++ peliculas/peliculas/lib/widgets/card_swiper.dart
@@ -8,14 +8,27 @@ class CardSwiper extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return Container(
+      padding: EdgeInsets.only(top: 20),
       width: double.infinity,
       height: 500,
-      color: Colors.red,
       child: Swiper(
         itemCount: 10,
         layout: SwiperLayout.STACK,
         itemWidth: size.width *0.7,
-        itemHeight: size.height * 0.6,),
+        itemHeight: size.height * 0.6,
+        itemBuilder: (_, int index){
+          return GestureDetector(
+            child: ClipRRect(
+            child: FadeInImage(
+            placeholder: AssetImage('assets/no-image.png'), 
+            image: NetworkImage('https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0'),
+            fit: BoxFit.fill
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          onTap: () => Navigator.pushNamed(context, 'details_screen'),
+          ); 
+        },),
     );
   }
 }
